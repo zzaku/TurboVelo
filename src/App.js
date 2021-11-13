2264f792e2d39fcc095b007a5ee34060417c2fa3
zzaku <enzo.makked@gmail.com>
2021-11-13 20:59:06 +0100

    Changed form

--- src/App.js
+++ src/App.js
@@ -1,53 +1,101 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import React, { useState } from 'react';
-import StatusClient from './Routes/StatusClient'
-import Identification from './Routes/Identification';
-import Profil from './Routes/Profil';
-import Navbar from './Routes/Navbar'
-import Reservation from './Routes/Reservation';
-import Reparation from './Routes/Reparation'
-import Repararer from './Routes/Reparer';
-import {Footer, Text, Anchor} from 'grommet'
-import {connectedContext, demandeContext} from './Components/MyContexts'
-import './App.css';
-import { useContext, useEffect } from 'react/cjs/react.development';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
+import StatusClient from "./Routes/StatusClient";
+import Identification from "./Routes/Identification";
+import Profil from "./Routes/Profil";
+import Navbar from "./Routes/Navbar";
+import Reservation from "./Routes/Reservation";
+import Reparation from "./Routes/Reparation";
+import Repararer from "./Routes/Reparer";
+import { Footer, Text, Anchor } from "grommet";
+import { connectedContext, demandeContext } from "./Components/MyContexts";
+import "./App.css";
+import { useContext, useEffect } from "react/cjs/react.development";
 
 function App() {
-  
-  const saveConnexion = localStorage.connected
+  const saveConnexion = localStorage.connected;
 
-  const [connected, setConnected] = useState(saveConnexion ? JSON.parse(saveConnexion) : [])
-  
-  useEffect(() => {
-      localStorage.setItem('connected', JSON.stringify(connected))
-    }, [connected])
-
-  const saveReparateur = localStorage.reparateur
-
-  const [reparateur, setReparateur] = useState(saveReparateur ? JSON.parse(saveReparateur) : [])
+  const [connected, setConnected] = useState(
+    saveConnexion ? JSON.parse(saveConnexion) : []
+  );
 
   useEffect(() => {
-    localStorage.setItem('reparateur', JSON.stringify(reparateur))
-  }, [reparateur])
+    localStorage.setItem("connected", JSON.stringify(connected));
+  }, [connected]);
 
-  
-  
+  const saveReparateur = localStorage.reparateur;
+
+  const [reparateur, setReparateur] = useState(
+    saveReparateur ? JSON.parse(saveReparateur) : []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("reparateur", JSON.stringify(reparateur));
+  }, [reparateur]);
+
   return (
     <connectedContext.Provider value={connected.id}>
-          <Router>
-            <Navbar connected={connected} reparateur={reparateur} setReparateur={setReparateur}/>
-            <Switch>
-                <Route path='/Home' exact component={() => <StatusClient connected={connected} />}></Route>
-                <Route path='/Profil' exact component={() => <Profil connected={connected} setConnected={setConnected} setReparateur={setReparateur}/>}></Route>
-                <Route path='/Reservation' exact component={() => <Reservation connected={connected}/>}></Route>
-                <Route path='/Reparation' exact component={() => <Reparation  connected={connected} setConnected={setConnected} reparateur={reparateur} setReparateur={setReparateur}/>}></Route>
-                <Route path='/Reparer' exact component={() => <Repararer  connected={connected} setConnected={setConnected}/>}></Route>
-                <Route path='/Connexion' exact component={() => <Identification  connected={connected} setConnected={setConnected}/>}></Route>
-            </Switch>
-          </Router>
-      </connectedContext.Provider>
-  )
+      <Router>
+        <Navbar
+          connected={connected}
+          reparateur={reparateur}
+          setReparateur={setReparateur}
+        />
+        <Switch>
+          <Route
+            path="/Home"
+            exact
+            component={() => <StatusClient connected={connected} />}
+          ></Route>
+          <Route
+            path="/Profil"
+            exact
+            component={() => (
+              <Profil
+                connected={connected}
+                setConnected={setConnected}
+                setReparateur={setReparateur}
+              />
+            )}
+          ></Route>
+          <Route
+            path="/Reservation"
+            exact
+            component={() => <Reservation connected={connected} />}
+          ></Route>
+          <Route
+            path="/Reparation"
+            exact
+            component={() => (
+              <Reparation
+                connected={connected}
+                setConnected={setConnected}
+                reparateur={reparateur}
+                setReparateur={setReparateur}
+              />
+            )}
+          ></Route>
+          <Route
+            path="/Reparer"
+            exact
+            component={() => (
+              <Repararer connected={connected} setConnected={setConnected} />
+            )}
+          ></Route>
+          <Route
+            path="/Connexion"
+            exact
+            component={() => (
+              <Identification
+                connected={connected}
+                setConnected={setConnected}
+              />
+            )}
+          ></Route>
+        </Switch>
+      </Router>
+    </connectedContext.Provider>
+  );
 }
 
 export default App;
-
--- src/Components/FormService.js
+++ src/Components/FormService.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './Styles/FormService.css'
-import {ToastApprouved} from "../Components/TitleAndMessageNotificationApprouved"
-import {Toast} from "../Components/TitleAndMessageNotification "
+import React, { useState } from "react";
+import "./Styles/FormService.css";
+import { ToastApprouved } from "../Components/TitleAndMessageNotificationApprouved";
+import { Toast } from "../Components/TitleAndMessageNotification ";
 
 import {
   Box,
@@ -17,59 +17,83 @@ import {
   Select,
   TextArea,
   TextInput,
-} from 'grommet';
-import { grommet } from 'grommet/themes';
+} from "grommet";
+import { grommet } from "grommet/themes";
 
-export const ControlledInput = ({childConnected, service, setService, dataService, setDataService, setNavReparateur}) => {
-  const [titre, setTitre] = useState('');
-  const [description, setDescription] = useState('');
-  const [tarif, setTarif] = useState('');
-  const [zone, setZone] = useState('');
-  console.log(service)
+export const ControlledInput = ({
+  childConnected,
+  service,
+  setService,
+  dataService,
+  setDataService,
+  setNavReparateur,
+}) => {
+  const [titre, setTitre] = useState("");
+  const [description, setDescription] = useState("");
+  const [tarif, setTarif] = useState("");
+  const [zone, setZone] = useState("");
+  console.log(service);
   return (
     <div className="form-recherche">
       <Box fill align="center" justify="center">
         <Box width="medium">
           <Form
-          onChange={event => setService({titre, description, tarif, zone})}
+            onChange={(event) =>
+              setService({ titre, description, tarif, zone })
+            }
             onReset={() => {
-              setTitre('');
-              setDescription('');
-              setTarif('');
-              setZone('');
+              setTitre("");
+              setDescription("");
+              setTarif("");
+              setZone("");
             }}
-            onSubmit={event =>
-                setService(event.value) + fetch('http://localhost/Back-end/AddService.php', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        childConnected,
-                        service
-                    })
-                }).then(res => res.json())
-                .then(data => setDataService(data)) + dataService.successAjout ? <><ToastApprouved /></> + setTimeout(function(){setNavReparateur('Services')},1500) : <Toast />
+            onSubmit={(event) =>
+              setService(event.value) +
+              fetch("http://localhost/Back-end/AddService.php", {
+                method: "POST",
+                body: JSON.stringify({
+                  childConnected,
+                  service,
+                }),
+              })
+                .then((res) => res.json())
+                .then((data) => setDataService(data)) +
+              dataService.successAjout ? (
+                (
+                  <>
+                    <ToastApprouved />
+                  </>
+                ) +
+                setTimeout(function () {
+                  setNavReparateur("Services");
+                }, 1500)
+              ) : (
+                <Toast />
+              )
             }
           >
             <div className="form-select">
-                        <p className="label-titre">Titre</p><br/>
-                        <Select
-                            options={['freins', 'pneus', 'revision', 'autre']}
-                            value={titre}
-                            onChange={({ option }) => setTitre(option)}
-                        />
-                    </div>
-                    <br/>
+              <p className="label-titre">Titre</p>
+              <br />
+              <Select
+                options={["freins", "pneus", "revision", "autre"]}
+                value={titre}
+                onChange={({ option }) => setTitre(option)}
+              />
+            </div>
+            <br />
             <FormField label="Description" name="description">
               <TextArea
                 name="description"
                 value={description}
-                onChange={event => setDescription(event.target.value)}
+                onChange={(event) => setDescription(event.target.value)}
               />
             </FormField>
             <FormField label="Tarif en €" name="tarif">
               <TextInput
                 name="tarif"
                 value={tarif}
-                onChange={event => setTarif(event.target.value)}
+                onChange={(event) => setTarif(event.target.value)}
               />
             </FormField>
             <FormField label="Zone" name="zone" pad>
@@ -78,14 +102,14 @@ export const ControlledInput = ({childConnected, service, setService, dataServic
                 min={5}
                 max={100}
                 value={zone}
-                onChange={event => setZone(event.target.value)}
+                onChange={(event) => setZone(event.target.value)}
               />
               {service.zone} km
             </FormField>
-            <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+            <Box direction="row" justify="between" margin={{ top: "medium" }}>
               <Button label="Annuler" />
               <Button type="reset" label="Effacer" />
-              <Button type='submit' label="Ajouter" primary />
+              <Button type="submit" label="Ajouter" primary />
             </Box>
           </Form>
         </Box>
@@ -94,7 +118,7 @@ export const ControlledInput = ({childConnected, service, setService, dataServic
   );
 };
 
-ControlledInput.storyName = 'Controlled input';
+ControlledInput.storyName = "Controlled input";
 
 ControlledInput.parameters = {
   // chromatic disabled because snapshot is the same as ControlledInputLazy
@@ -102,5 +126,5 @@ ControlledInput.parameters = {
 };
 
 export default {
-  title: 'Input/Form/Controlled input',
+  title: "Input/Form/Controlled input",
 };
